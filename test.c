bd81846ad4a5f67be9c4b2cd4f8745fe71b9dfbc
hjk0314 <hjk0314@gmail.com>
2023-03-27 19:21:08 +0900

    float, double print

--- test.c
+++ test.c
@@ -8,6 +8,15 @@ int main(void)
     unsigned short int usi; /* 0 ~ 65536 */
     signed long int sli; /* -2147483648 ~ 2147483647 */
     unsigned long int uli; /* 0 ~ 4294967296 */
-    printf("sc: %d\n", sc);
+    float f = 3.14f; /* 기본적으로 여섯자리까지 표현 */
+    double d = 3.141592654; /* 더블도 기본적으로 여섯자리까지 표현 */
+    printf("signed char: %d\n", sc);
+    printf("unsigned char: %d\n", uc);
+    printf("signed short int: %d\n", ssi);
+    printf("unsigned short int: %d\n", usi);
+    printf("signed long int: %d\n", sli);
+    printf("unsigned long int: %d\n", uli);
+    printf("float: %f\n", f);
+    printf("double: %.9lf\n", d); /* 9자리 모두 표현하는 방법 */
     return 0;
 }
